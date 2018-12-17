ff539717318f0b5261ace63846e7e275b2020e64
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2018-12-17 02:58:32 -0800

    removed notes because machine is being a butthole

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -405,12 +405,4 @@ $(document).ready(function () {
             start();
            });
        }
-//sunday- fixed  initial RESTART PROBLEM 403
-//2nd restart problem- Character doesn't change.
-//fixing 343-346, choose next fighter
-//increased attack power
-//to show outcomes.
-//removed 243, this allows game to flow through, 
-//but text is doubled, no 2nd attack from Opponent
-//commented out 276-290. Didn't fix error but didnt crash!
 });
